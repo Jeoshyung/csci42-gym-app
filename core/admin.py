0a51a8ecfea089940deab6dadf6fa667f0310023
AdrMrcs <adrian.villanueva@student.ateneo.edu>
2025-03-04 18:29:16 +0800

    Backend Setup

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Exercise, ExerciseCategory, MuscleGroup, Muscle, Equipment
 
 # Register your models here.
+
+admin.site.register(Exercise)
+admin.site.register(ExerciseCategory)
+admin.site.register(MuscleGroup)
+admin.site.register(Muscle)
+admin.site.register(Equipment)
