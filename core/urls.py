7154ed36cbd7e9ac67571caef0c90feab5175170
Kenjie Go <157807351+Kenj232@users.noreply.github.com>
2025-03-13 22:34:09 +0800

    Looks like Figma
    Change templates to look more like figma added forgot password and remember me

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import login_view, register_view, index_view
 from django.contrib.auth.views import LogoutView
 
@@ -7,4 +8,6 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', index_view, name='index'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
 ]
+from django.contrib.auth import views as auth_views
