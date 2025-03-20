33ca6ddc3e4b7010ca2f589d63a669de117361de
AdrMrcs <adrian.villanueva@student.ateneo.edu>
2025-03-20 22:52:09 +0800

    added workout logging

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import WorkoutSession, WorkoutLogging, Profile
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150)
@@ -12,3 +13,18 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class WorkoutSessionForms(forms.ModelForm):
+    class Meta:
+        model = WorkoutSession
+        fields = ['notes']
+
+class WorkoutLoggingForm(forms.ModelForm):
+    class Meta:
+        model = WorkoutLogging
+        fields = ['exercise', 'sets', 'reps']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'email']
