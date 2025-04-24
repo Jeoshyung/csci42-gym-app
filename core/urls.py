a8efba51e6307ba3dc9104f8c91c1938e32c55bd
bfrancisco <franciscobryaaan@gmail.com>
2025-04-24 23:00:21 +0800

    adjust urls and views for password_reset

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import login_view, register_view, index_view, profile_view, workouts_view, workout_logger_view, add_goal_view, add_personal_record_view, exercise_detail_view, notifications_view, profile_setup_view
+from .views import login_view, register_view, index_view, profile_view, workouts_view, workout_logger_view, add_goal_view, add_personal_record_view, exercise_detail_view, notifications_view, profile_setup_view, password_reset_view
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
@@ -10,8 +10,9 @@ urlpatterns = [
      path('', index_view, name='index'),
      path("profile/", profile_view, name="profile"),
      path('workoutlogging/', workout_logger_view, name='workout_logger'),
-     path('password-reset/', auth_views.PasswordResetView.as_view(),
-          name='password_reset'),
+     # path('password-reset/', auth_views.PasswordResetView.as_view(),
+     #      name='password_reset'),
+     path('password-reset/', password_reset_view, name='password_reset'),
      path('workouts/', workouts_view, name='workouts'),
      path('add-goal/', add_goal_view, name='add_goal'),
      path("add-personal-record/", add_personal_record_view,
